bdc7fbc48309706c5b966deb045c85e2ff81fb81
Jeffrey Varner <jdv27@cornell.edu>
2021-06-02 09:09:09 -0400

    Initial commit
    Initial commit

---
+++ Include.jl
@@ -0,0 +1,39 @@
+# ----------------------------------------------------------------------------------- #
+# Copyright (c) 2021 Varnerlab
+# Robert Frederick Smith School of Chemical and Biomolecular Engineering
+# Cornell University, Ithaca NY 14850
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the &quot;Software&quot;), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and&#x2F;or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# ----------------------------------------------------------------------------------- #
+
+
+# setup paths -
+const _PATH_TO_ROOT = pwd()
+const _PATH_TO_SRC = joinpath(_PATH_TO_ROOT, "src")
+
+# use packages -
+using DifferentialEquations
+using VLModelParametersDB
+using DelimitedFiles
+
+# include my codes -
+include("./src/Parameters.jl")
+include("./src/Balances.jl")
+include("./src/Kinetics.jl")
+include("./src/Control.jl")
---
+++ Initialize.jl
@@ -0,0 +1,41 @@
+# ----------------------------------------------------------------------------------- #
+# Copyright (c) 2021 Varnerlab
+# Robert Frederick Smith School of Chemical and Biomolecular Engineering
+# Cornell University, Ithaca NY 14850
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the &quot;Software&quot;), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and&#x2F;or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# ----------------------------------------------------------------------------------- #
+# stuff we need -
+import Pkg
+
+# initialization function -
+function __init__()
+    
+    # setup paths -
+    _PATH_TO_ROOT = pwd()
+
+    # get packages -
+    Pkg.activate(_PATH_TO_ROOT)
+    Pkg.add(name="DifferentialEquations")
+    Pkg.add(name="VLModelParametersDB")
+    Pkg.add(name="DelimitedFiles")
+end
+
+# initialize me -
+__init__()
